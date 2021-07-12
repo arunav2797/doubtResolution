fb85fd758a96b5d8a97f535f616a652b9ab883ef
Arunav Saxena <arunavsaxena27@gmail.com>
2021-07-13 01:54:27 +0530

    Update 20210711113531_create_comments.rb

--- db/migrate/20210711113531_create_comments.rb
+++ db/migrate/20210711113531_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.string :comment, null: false
-      t.string email, null: false
+      t.string :email, null: false
       t.timestamps
     end
   end
