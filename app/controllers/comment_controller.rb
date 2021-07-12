4dc31bb7746ddaf40998f8a18ac75f98d176792e
Arunav Saxena <arunavsaxena27@gmail.com>
2021-07-12 14:21:51 +0530

    general changes

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,4 +1,5 @@
 class CommentController < ApplicationController
+  before_action :require_user_logged_in!
   def add
     @comment = Comment.new(comment: params[:comment], user_id: session[:user_id], doubt_id: params[:doubt_id], email: session[:email])
     if @comment.save
--- app/controllers/doubt_controller.rb
+++ app/controllers/doubt_controller.rb
@@ -1,42 +1,52 @@
 class DoubtController < ApplicationController
+  before_action :require_user_logged_in!
   def doubtForm; end
   def raise_doubt
-    @doubt = Doubt.new(title: params[:title], description: params[:description], user_id: session[:user_id], status: "Submitted")
-    respond_to do |format|
-      if @doubt.save
-        @doubt_association = TaDoubt.new(doubt_id: @doubt.id)
-        if @doubt_association.save
-          format.html { redirect_to home_path, notice: 'Doubt raised successdfully' }
+    if session[:role] == "Student"
+      @doubt = Doubt.new(title: params[:title], description: params[:description], user_id: session[:user_id], status: "Submitted")
+      respond_to do |format|
+        if @doubt.save
+          @doubt_association = TaDoubt.new(doubt_id: @doubt.id)
+          if @doubt_association.save
+            format.html { redirect_to home_path, notice: "Doubt raised successdfully" }
+          end
         end
       end
     end
   end
 
   def accept_doubt
-    @doubt = Doubt.find(params[:doubt_id])
-    @doubt.update(status: "Accepted")
-    redirect_to doubt_detail_path(doubt_id: params[:doubt_id])
+    if session[:role] == "TA"
+      @doubt = Doubt.find(params[:doubt_id])
+      @doubt.update(status: "Accepted")
+      redirect_to doubt_detail_path(doubt_id: params[:doubt_id])
+    end
   end
 
   def doubt_detail
+    if session[:role] == "TA"
       @doubt = Doubt.find(params[:doubt_id])
       render "doubtDetail"
+    end
   end
 
   def answer_doubt
-    @doubt_assoc = TaDoubt.where(doubt_id: params[:doubt_id])
-    @doubt_assoc.update(user_id: session[:user_id])
-    @doubt = Doubt.where(id: params[:doubt_id])
-    @doubt.update(solution: params[:solution], status: "Reviewed")
-    redirect_to home_path
+    if session[:role] == "TA"
+      @doubt_assoc = TaDoubt.where(doubt_id: params[:doubt_id])
+      @doubt_assoc.update(user_id: session[:user_id])
+      @doubt = Doubt.where(id: params[:doubt_id])
+      @doubt.update(solution: params[:solution], status: "Reviewed")
+      redirect_to home_path
+    end
   end
 
   def escalate_doubt
-    @doubt_assoc = TaDoubt.where(doubt_id: params[:doubt_id])
-    @id = User.where(role: "TA").and(User.where.not(id: session[:user_id])).pluck(:id).sample
-    @doubt_assoc.update(user_id: @id)
-    Doubt.update(status: "Escalated").where(id: params[:doubt_id])
-    redirect_to home_path
+    if session[:role] == "TA"
+      @doubt_assoc = TaDoubt.where(doubt_id: params[:doubt_id])
+      @id = User.where(role: "TA").and(User.where.not(id: session[:user_id])).pluck(:id).sample
+      @doubt_assoc.update(user_id: @id)
+      Doubt.update(status: "Escalated").where(id: params[:doubt_id])
+      redirect_to home_path
+    end
   end
-
 end
